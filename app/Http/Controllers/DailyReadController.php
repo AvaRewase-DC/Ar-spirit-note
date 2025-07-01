a52672eed6e8cdf5d384385f4f4248bba409239f
micheal <micheal.s.samir@gmail.com>
2025-07-01 15:00:57 +0300

    clean

--- app/Http/Controllers/DailyReadController.php
+++ app/Http/Controllers/DailyReadController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\DailyRead;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 
 class DailyReadController extends Controller
 {
@@ -13,8 +12,9 @@ class DailyReadController extends Controller
         $startOfMonth = Carbon::create(now()->year, now()->month, 1)->startOfDay();
         $endOfMonth = Carbon::create(now()->year, now()->month, 1)->endOfMonth()->endOfDay();
         $reads = DailyRead::whereDate('day', '>=', $startOfMonth)
-                        ->whereDate('day', '<=', $endOfMonth)
-                        ->get();
+            ->whereDate('day', '<=', $endOfMonth)
+            ->get();
+
         return view('daily-read.index', compact('reads'));
     }
 }
